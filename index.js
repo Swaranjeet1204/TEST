201ad6a31624eb7b75d00ce1706293b6cf14a6cb
Swaranjeet1204 <suriswaranjeet@gmail.com>
2024-06-05 09:23:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// let a=parseInt(prompt("eanter a number"));
+// let b=200
+
+// let c=a+b
+
+// alert(c)
+
+
+
+
+let age=parseInt(prompt("Enter Your birth year"));
+if(age<2000){
+    alert("you are adult ");
+}
+else{
+   alert ("you are child ");
+}
